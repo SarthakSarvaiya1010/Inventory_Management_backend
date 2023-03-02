ea42b0b28c716ed46a96fbb78ba13fde3e1a014e
Karan Talapalli <karan.t@volgainfotech.com>
2023-03-02 18:50:56 +0530

    check image eroor

--- src/helpers/imageUploader.js
+++ src/helpers/imageUploader.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log("req", req);
     console.log("🚀 ~ file: upload.ts:11 ~ file");
     cb(null, `${process.cwd()}/public/images`);
     // cb(null, "public/images");
