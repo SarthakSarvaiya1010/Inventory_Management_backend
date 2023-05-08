6f507086e48ed219e13023bd742a65fe669e87e7
Karan Talapalli <karan.t@volgainfotech.com>
2023-05-08 16:07:41 +0530

    update date filter

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -2,5 +2,5 @@ exports.convert = (str) => {
   var date = new Date(str),
     mnth = ("0" + (date.getMonth() + 1)).slice(-2),
     day = ("0" + date.getDate()).slice(-2);
-  return [day, mnth, date.getFullYear()].join("-");
+  return [mnth, day, date.getFullYear()].join("-");
 };
