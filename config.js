8a208e8506cb00f2a7cd29b65ae6e248ea13e7fc
SarthakSarvaiya <sarthak.sarvaiya@Aimsinfosoft.com>
2025-06-25 17:39:58 +0530

    update config

--- config.js
+++ config.js
@@ -3,22 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false, // ✅ Fix here
+  ssl: isProduction,
 });
-
-pool.connect()
-  .then(() => {
-    console.log("✅ Database connected successfully");
-    console.log("DB String :-", connectionString);
-  })
-  .catch((err) => {
-    console.log("DB String :-", connectionString);
-    console.error("❌ Database connection failed:", err.message);
-    process.exit(1);
-  });
 
 module.exports = pool;
